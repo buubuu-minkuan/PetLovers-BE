714003e0243c18c79f306bba4be4f0e075659e43
Đoàn Ngọc Minh Quân <minhquandoanngoc@gmail.com>
2023-10-30 00:12:54 +0700

    update update user

--- Data/Models/UserModel/UserModel.cs
+++ Data/Models/UserModel/UserModel.cs
@@ -87,9 +87,10 @@ namespace Data.Models.UserModel
 
     public class UserUpdateReqModel
     {
-        public string Username { get; set; }
         public string Name { get; set; }
-
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string? Image { get; set; }
     }
 
     public class UserFollowingModel
